bdc3362a4515bb342f9848cc952917047d1ac26e
Md. Rakib Mah <99381836+rakibraz43@users.noreply.github.com>
2022-04-15 19:25:13 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
